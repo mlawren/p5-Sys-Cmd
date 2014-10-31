57b8e20c5c7f6f379c0065f27a5b6d6cf50bb82f
Mark Lawrence <nomad@null.net>
2014-10-31 17:58:07 +0100

    Unset release date

--- lib/Sys/Cmd.pm
+++ lib/Sys/Cmd.pm
@@ -500,7 +500,7 @@ Sys::Cmd - run a system command or spawn a system processes
 
 =head1 VERSION
 
-0.81.8 (2014-10-31) Development release
+0.81.8 (yyyy-mm-dd) Development release
 
 =head1 SYNOPSIS
 
--- lib/Sys/Cmd/Template.pm
+++ lib/Sys/Cmd/Template.pm
@@ -152,7 +152,7 @@ Sys::Cmd::Template - command/process templates for Sys::Cmd
 
 =head1 VERSION
 
-0.81.8 (2014-10-31) Development release
+0.81.8 (yyyy-mm-dd) Development release
 
 =head1 SYNOPSIS
 
