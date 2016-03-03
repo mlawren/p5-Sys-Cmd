7eaa169121eb8a2951b586a22f3158edd3bae132
Mark Lawrence <nomad@null.net>
2016-03-03 21:30:51 +0100

    Release version 0.85.1_1 (2016-03-03)

--- lib/Sys/Cmd/Template.pm
+++ lib/Sys/Cmd/Template.pm
@@ -152,7 +152,7 @@ Sys::Cmd::Template - command/process templates for Sys::Cmd
 
 =head1 VERSION
 
-0.85.1_1 (yyyy-mm-dd) Development release
+0.85.1_1 (2016-03-03) Development release
 
 =head1 SYNOPSIS
 
