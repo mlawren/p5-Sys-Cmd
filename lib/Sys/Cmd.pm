c95bfb2e1af9f78c71bf6e19c0334e12d3ad7cb5
Mark Lawrence <mark@rekudos.net>
2025-03-31 22:06:43 +0200

    Set version to 0.985.0

--- lib/Sys/Cmd.pm
+++ lib/Sys/Cmd.pm
@@ -15,7 +15,7 @@ use Exporter::Tidy _map => {
     spawnsub => sub { syscmd( undef, @_ )->spawnsub },
 };
 
-our $VERSION = '0.99.2_1';
+our $VERSION = '0.985.0';
 
 use Class::Inline {
     cmd => {
@@ -191,7 +191,7 @@ sub spawnsub {
 }
 
 package Sys::Cmd::Process;
-our $VERSION = '0.99.2_1';
+our $VERSION = '0.985.0';
 use parent -norequire, 'Sys::Cmd';
 use Encode 'encode';
 use IO::Handle;
@@ -565,7 +565,7 @@ Sys::Cmd - run a system command or spawn a system processes
 
 =head1 VERSION
 
-0.99.2_1 (yyyy-mm-dd)
+0.985.0 (yyyy-mm-dd)
 
 =head1 SYNOPSIS
 
